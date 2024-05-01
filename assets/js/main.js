1740d2d9d0a941a090880b4970bf63b0a89c18b7
Adil Naib <75264739+adil200@users.noreply.github.com>
2024-05-01 14:38:49 +0530

    Updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const typeWriter = (textElement, words, wait = 3000) => {
   let wordIndex = 0;
   let textIndex = 0;
-  let typingSpeed = 100; // Increase typing speed
+  let typingSpeed = 190; // Increase typing speed
   let eraseSpeed = 60; // Increase erasing speed
   let isDeleting = false;
 
@@ -19,11 +19,11 @@ const typeWriter = (textElement, words, wait = 3000) => {
 
     if (!isDeleting && textIndex === currentWord.length + 1) {
       isDeleting = true;
-      typingSpeed = 100; // Change speed for erasing
+      typingSpeed = 190; // Change speed for erasing
     } else if (isDeleting && textIndex === 0) {
       isDeleting = false;
       wordIndex = (wordIndex + 1) % words.length;
-      typingSpeed = 100; // Reset typing speed
+      typingSpeed = 190; // Reset typing speed
     }
 
     setTimeout(type, typingSpeed);
